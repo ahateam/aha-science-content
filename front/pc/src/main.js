ef781cde5c56157d6274c282a1947ffa5d2bb77b
weiyuanGit <16555749587@qq.com>
2019-11-12 09:05:22 +0800

    temp

--- front/pc/src/main.js
+++ front/pc/src/main.js
@@ -13,6 +13,8 @@ import Distpicker from 'v-distpicker'
 
 import BaiduMap from 'vue-baidu-map'
 
+import vcolorpicker from 'vcolorpicker'
+Vue.use(vcolorpicker)
 
 
 Vue.use(ElementUI)
@@ -20,6 +22,7 @@ Vue.use(BaiduMap, {
 	// ak 是在百度地图开发者平台申请的密钥 详见 http://lbsyun.baidu.com/apiconsole/key */
 	ak: 'YOUR_APP_KEY'
 })
+
 Vue.config.productionTip = false
 Vue.prototype.$constData = constData
 Vue.prototype.$util = util
