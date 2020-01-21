648a8172e8aedb39bf4a1087d12e6b3cb90a0fea
PS2019GSGQMEQK\Admin <1655749587@qq.com>
2020-01-21 13:44:52 +0800

    temp

--- front/pc/src/commen/oss/ossAuth.js
+++ front/pc/src/commen/oss/ossAuth.js
@@ -6,14 +6,7 @@
 const OSS = require('ali-oss');
 
 
-let client = new OSS({
-    region: 'oss-cn-chengdu',
-    //云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，部署在服务端使用RAM子账号或STS，部署在客户端使用STS。
-    accessKeyId: '',
-    accessKeySecret: '',
-    bucket: 'dzkx-kepu',
-	timeout: '1200000',
-});
+
 
 export default {
     client,
